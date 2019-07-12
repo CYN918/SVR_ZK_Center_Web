a7d076f10be4cfa03b613767ee91759961e808b1
zhang hui <644946418@qq.com>
2019-07-12 15:14:03 +0800

    no message

--- src/views/workbench/Add_the_resource.vue
+++ src/views/workbench/Add_the_resource.vue
@@ -9,7 +9,7 @@
                 </div>
                 <div class="title_div">
                     <span class="title_div_span">已添加：</span>
-                    <span class="title_div_span">{{this.total}}</span>
+                    <span class="title_div_span">{{this.numAll}}</span>
                     <span class="title_div_span">/</span>
                     <span class="title_div_span">{{num}}</span>
                     <span class="title_div_btn" @click="ADDline">
@@ -98,6 +98,7 @@
                 line_num:'',
                 mfid:'',
                 material:1,
+                numAll:0,
                 note:'',
                 line:[],
             }
@@ -259,6 +260,7 @@
                         this.listWL = res.data.mfinal;
                         this.total = res.total
                     }
+                    this.numAll=res.data.demand.hire_num;
                 })
             },
             addNote(index){
@@ -272,7 +274,7 @@
                 })
             },
             verified(){
-                if(this.total==this.num){
+                if(this.numAll==this.num){
                     let formData = new FormData;
                     formData.append('id',this.id);
                     this.api.demand_audit(formData).then((res)=>{
