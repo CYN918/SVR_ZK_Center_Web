b2d97a5e328037e2ed370591e64d7776d07470c4
644946418@qq.com <644946418@qq.com>
2020-04-20 18:30:19 +0800

    no message

--- src/views/workbench/news.vue
+++ src/views/workbench/news.vue
@@ -3,15 +3,24 @@
         <div class="top_name">
             <span class="top_txt">TX新闻模板管理</span>
         </div>  
-        <div>
+        <div class='screen'>
             <span>模板ID：</span>
             <input type="text">
             <span>模板名称：</span>
             <input type="text">
             <span>尺寸：</span>
-            <input type="text">
+            <select name="" id="">
+                <option value="">全部</option>
+            </select>
             <span>状态：</span>
-            <input type="text">
+            <select name="" id="">
+                <option value="">全部</option>
+            </select>
+            <div>
+                <span>查询</span>
+                <span>重置</span>
+                <span>新增模板</span>
+            </div>
         </div>
     </div>
 </template>
@@ -41,5 +50,26 @@
     font-weight:500;
     line-height:62px ;
     color:rgba(31,46,77,1);
+}
+.screen{
+    margin-top: 145px;
+    background: #fff;
+    height: 100px;
+}
+.screen span{
+    margin:0 10px 0 24px;
+    font-size: 14px;
+    line-height: 100px;
+}
+.screen input{
+    width: 200px;
+    height: 32px;
+    padding-left: 5px;
+    border-radius: 3px;
+}
+.screen select{
+    width: 120px;
+    height: 36px;
+    border-radius: 3px;
 }
 </style>
