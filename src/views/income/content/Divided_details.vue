955d654ac05c5986e343d71ae469cc9ac6bbeee5
644946418@qq.com <644946418@qq.com>
2020-05-18 15:58:08 +0800

    no message

--- src/views/income/content/Divided_details.vue
+++ src/views/income/content/Divided_details.vue
@@ -8,7 +8,7 @@
             <div class="title_left">
                 <span>分成详情</span>
                 <span class='time'>{{this.$route.query.tdate}}</span>
-                <span class='zt' :class='{red:this.status==0}'>({{this.status==0?"待确认":this.status==1?'审核中':this.status==2?'已确认':this.status==-1?'驳回':''}})</span>
+                <span class='zt' :class='{red:this.status==0||this.status==-1}'>({{this.status==0?"待确认":this.status==1?'审核中':this.status==2?'已确认':this.status==-1?'驳回':''}})</span>
             </div>
         </div>
         <div class='content'>
@@ -443,7 +443,7 @@ export default {
         font-weight: 400!important; 
     }
     .red{
-        color: red;
+        color: red!important;
     }
 
 
