09f5ed0d61d3b521ef79ae92b1f91c208634d11f
644946418@qq.com <644946418@qq.com>
2020-05-21 14:35:08 +0800

    no message

--- src/views/income/Advertising.vue
+++ src/views/income/Advertising.vue
@@ -326,7 +326,6 @@
                             }else{
                                 this.JSname=res.data;
                             }
-                            
                 })
                 }
                
