6d8598e0fe8a36e6ae7c6e903b11414b7daefefc
644946418@qq.com <644946418@qq.com>
2020-05-26 12:08:41 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -36,9 +36,9 @@ a{
 img{
   vertical-align: initial!important;
 }
-/* *, *::before, *::after{
+*, *::before, *::after{
   box-sizing:initial!important
-} */
+}
 .el-date-editor .el-range-separator{
   width: 7%;
 }
